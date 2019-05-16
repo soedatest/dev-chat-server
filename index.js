b174f92ba139e2f61ca902294f4629ecdf7a7506
dev <dev@example.com>
2019-05-16 18:14:44 +0900

    add socket process

--- index.js
+++ index.js
@@ -1,7 +1,16 @@
 const express = require('express')
 const app = express()
-app.get('/', function(req, res){
-  console.log('hello world')
-  res.send('hello world')
+
+const date = new Date()
+
+const server = app.listen(3000, function() {
+  console.log('listen 3000')
 })
-const server = app.listen(3000)
+const io = require('socket.io')(server)
+io.on('connection', function(socket){
+  console.log(socket.id)
+  socket.on('SEND', function(data){
+    console.log(date.getHours() + ':' + date.getMinutes() + ':' + date.getSeconds() + ' [username]' + data.username + '[message]' + data.message)
+    io.emit('MESSAGE', data)
+  })
+})
