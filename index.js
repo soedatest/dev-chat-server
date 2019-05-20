d54f4ab5d98f797087a28869749c36abc61e332a
dev <dev@example.com>
2019-05-21 03:25:09 +0900

    ADD Redis Connect + TestRun

--- index.js
+++ index.js
@@ -3,6 +3,28 @@ const app = express()
 
 const date = new Date()
 
+// Redis Connect
+const redis = require('redis')
+const redisConfig = {
+  host: '0.0.0.0',
+  port: 6379,
+  password: 'password',
+  db: 1
+}
+const redisClient = redis.createClient(redisConfig)
+// Insert Test
+const key = date.getHours() + ':' + date.getMinutes() + ':' + date.getSeconds()
+const value = {user:'A', message:'Hello'}
+redisClient.set(key, JSON.stringify(value), function(){
+  console.log('insert')
+})
+// Select Test
+redisClient.get(key, function(err, val){
+  if (err) return console.log(err)
+  console.log(val)
+  console.log(JSON.parse(val))
+})
+
 const server = app.listen(3000, function() {
   console.log('listen 3000')
 })
